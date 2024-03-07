bda03e8614bf09ac9cf11208a029bcbc21df6742
lucasedusi <lucasmto.16@hotmail.com>
2024-03-07 15:34:15 -0300

    :lipstick: feat: update global style

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+	colors: {
+		"base-white": "#FFFFFF",
+		"base-background": "#FAFAFA",
+		"base-card": "#F3F2F2",
+		"base-error": "#f84747",
+		"brand-purple-dark": "#4B2995",
+		"brand-purple": "#8047F8",
+	},
+	textSizes: {
+		"title-title-xl": "3rem",
+		"title-title-l": "2rem",
+		"title-title-m": "1.5rem",
+		"title-title-s": "1.2rem",
+		"title-title-xs": "1.125rem",
+		"text-regular-l": "1.25rem",
+		"text-bold-l": "1.25rem",
+		"text-regular-m": "1rem",
+	},
+	fonts: {
+		regular: "'Roboto'",
+		title: "'Baloo 2'",
+	},
+};
